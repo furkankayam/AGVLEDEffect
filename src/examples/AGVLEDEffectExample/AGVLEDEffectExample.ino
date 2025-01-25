1abc0c0143b035828843dd104a4819197c4f732e
Furkan KAYA <97371955+furkankayam@users.noreply.github.com>
2025-01-26 02:48:21 +0300

    feat: update AGVLEDEffectExample.ino

--- src/examples/AGVLEDEffectExample/AGVLEDEffectExample.ino
+++ src/examples/AGVLEDEffectExample/AGVLEDEffectExample.ino
@@ -3,9 +3,9 @@
     Date: 26/01/2025
 */
 
-#include <AGVLEDEffect.h>
+#include <AGVLEDEffect.h> // Include the AGVLEDEffect library
 
-// LED effect object is already created in the library
+// AGVLEDEffect object
 AGVLEDEffect ledEffect;
 
 void setup() {
@@ -14,33 +14,33 @@ void setup() {
 void loop() {
   // Charge effect
   ledEffect.chargingEffect();
-  delay(2000);
+  delay(5000);
 
   // Moving to charge station effect
   ledEffect.movingToChargeStationEffect();
-  delay(2000);
+  delay(5000);
 
   // Loading effect
   ledEffect.loadEffect();
-  delay(2000);
+  delay(5000);
 
   // Unloading effect
   ledEffect.unloadEffect();
-  delay(2000);
+  delay(5000);
 
   // Right fill effect
   ledEffect.rightFillEffect();
-  delay(2000);
+  delay(5000);
 
   // Left fill effect
   ledEffect.leftFillEffect();
-  delay(2000);
+  delay(5000);
 
   // Go effect
   ledEffect.goEffect();
-  delay(2000);
+  delay(5000);
 
   // Stop effect
   ledEffect.stopEffect();
-  delay(2000);
+  delay(5000);
 }
